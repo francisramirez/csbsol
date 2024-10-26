a02e0adf16c3c1a782681a6bd1c89e2ace9adba6
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2024-10-26 17:03:45 -0400

    ok

---
+++ CSB/CacularSalario/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+    
+       
+        CalcularSalario();
+    }
+
+    /// <summary>
+    /// Calcular el salario de un empleado.
+    /// </summary>
+    private static void CalcularSalario()
+    {
+        // Declarar sus variables de entrada //
+        int num1 = 0;
+        int num2 = 0;
+        int suma = 0;
+        int producto = 0;
+       
+
+        //Pedir numeros por teclados
+        Console.WriteLine("Escriba el valor del num 1: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        //int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escriba el valor del num 2: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        //Calcular la suma y el producto.
+        suma = (num1 + num2);
+        producto = (num1 * num2);
+
+
+        // Imprimir valores //
+        Console.WriteLine($"La suma es: { suma } y el producto es: { producto }");
+    }
+}
