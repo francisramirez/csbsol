55d6841b19f85ff80a720927d26302635e6f9e6b
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2024-11-05 19:03:31 -0400

    ok.

--- CSB/CacularSalario/Program.cs
+++ CSB/CacularSalario/Program.cs
@@ -5,9 +5,8 @@ internal class Program
 
     private static void Main(string[] args)
     {
-    
-       
-        CalcularSalario();
+        //CalcularSalario();
+        MayorDeDosNumeros();
     }
 
     /// <summary>
@@ -15,12 +14,58 @@ internal class Program
     /// </summary>
     private static void CalcularSalario()
     {
+        decimal sueldo = 0;
+        decimal impuesto = 0;
+        decimal itbis = 0.20m;
+        decimal totalAPagar = 0;
+        string? valor = string.Empty;
+        decimal sueldoBase = 5000;
+
+        // Pedimos el sueldo
+
+        Console.WriteLine("Ingrese el sueldo del empleado.");
+        valor = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(valor))
+        {
+            Console.WriteLine($"El sueldo no puede ser nulo.");
+            return;
+        }
+
+
+        if (!decimal.TryParse(valor, out sueldo))
+        {
+            Console.WriteLine($"El sueldo {valor} no es válido.");
+            return;
+        }
+
+        if (sueldo < 0)
+        {
+            Console.WriteLine($"El sueldo no puede ser negativo.");
+            return;
+        }
+
+        if (sueldo > sueldoBase)
+        {
+            impuesto = (sueldo * itbis);
+            totalAPagar = (sueldo - impuesto);
+            Console.WriteLine($"impuesto: {impuesto} y sueldo después de impuesto: {totalAPagar}");
+        }
+        else
+        {
+            Console.WriteLine($"se le paga su salario completo.");
+        }
+
+    }
+
+    public static void CalcularSumaProducto()
+    {
         // Declarar sus variables de entrada //
         int num1 = 0;
         int num2 = 0;
         int suma = 0;
         int producto = 0;
-       
+
 
         //Pedir numeros por teclados
         Console.WriteLine("Escriba el valor del num 1: ");
@@ -36,6 +81,62 @@ internal class Program
 
 
         // Imprimir valores //
-        Console.WriteLine($"La suma es: { suma } y el producto es: { producto }");
+        Console.WriteLine($"La suma es: {suma} y el producto es: {producto}");
+    }
+
+    public static void MayorDeDosNumeros()
+    {
+        // Declarar sus variables de entrada //
+        int num1 = 0;
+        int num2 = 0;
+        string valor = string.Empty;
+
+        //Pedir numeros por teclados
+        Console.WriteLine("Escriba el valor del num 1: ");
+        valor = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(valor))
+        {
+            Console.WriteLine("El num1 es requerido.");
+            return;
+        }
+
+        if (!int.TryParse(valor, out num1))
+        {
+            Console.WriteLine("El num1 es inválido.");
+            return;
+        }
+
+        //Pedir numeros por teclados
+        Console.WriteLine("Escriba el valor del num 2: ");
+        valor = Console.ReadLine();
+
+
+        if (string.IsNullOrWhiteSpace(valor))
+        {
+            Console.WriteLine("El num2 es requerido.");
+            return;
+        }
+
+        if (!int.TryParse(valor, out num2))
+        {
+            Console.WriteLine("El num2 es inválido.");
+            return;
+        }
+
+        string message = (num1 > num2) ? $"El mayor es: { num1 }" : $"El mayor es: { num2 }";
+
+        Console.WriteLine(message);
+
+        //if (num1 > num2)
+        //{
+        //    Console.WriteLine($"El mayor es: {num1}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"El mayor es: {num2}");
+        //}
+
+
     }
 }
--- CSB/CacularSalario/obj/Debug/net8.0/CacularSalario.AssemblyInfo.cs
+++ CSB/CacularSalario/obj/Debug/net8.0/CacularSalario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CacularSalario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ccfb6c3bbdfbe342fefe486ee721a780deec9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CacularSalario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CacularSalario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
